7ed859503ad4bd28a5cba5e7d1f302dbb3770cba
Ian Blenke <ian@blenke.com>
2018-12-13 19:13:00 -0500

    Change the tilde to HOME

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ GITHUB_USERS=${GITHUB_USERS:-$@}
 
 # Install dependencies
 which bash || pkg install bash
+which curl || pkg install curl
 which git || pkg install git
 which rsync || pkg install rsync
 which sshd || pkg install openssh
@@ -13,35 +14,36 @@ which tor || pkg install tor
 if [ -n "$GITHUB_USERS" ]; then
 
   # Add ssh keys from github user ianblenke
-  touch ~/.ssh/authorized_keys
+  touch $HOME/.ssh/authorized_keys
   (
-    cat ~/.ssh/authorized_keys
+    cat $HOME/.ssh/authorized_keys
     for user in $GITHUB_USERS; do
       curl -sL https://github.com/${user}.keys
     done
-  ) | sort > ~/.ssh/authorized_keys.new
-  mv ~/.ssh/authorized_keys.new ~/.ssh/authorized_keys
-  chmod 700 ~/.ssh
-  chmod 600 ~/.ssh/authorized_keys
+  ) | sort > $HOME/.ssh/authorized_keys.new
+  mv $HOME/.ssh/authorized_keys.new ~/.ssh/authorized_keys
+  chmod 700 $HOME/.ssh
+  chmod 600 $HOME/.ssh/authorized_keys
 
 fi
 
-if [ ! -d ~/termux-tor-ssh ]; then
-  git clone https://github.com/ianblenke/termux-tor-ssh ~/termux-tor-ssh
+if [ ! -d $HOME/termux-tor-ssh ]; then
+  git clone https://github.com/ianblenke/termux-tor-ssh $HOME/termux-tor-ssh
 fi
 
-cd ~/termux-tor-ssh
-rsync -SHPaxq .bash_profile ~/.bash_profile
-rsync -SHPaxq .sv/ ~/.sv
-rsync -SHPaxq usr/ ~/../usr/
+cd $HOME/termux-tor-ssh
+rsync -SHPaxq .bash_profile $HOME/.bash_profile
+rsync -SHPaxq .sv/ $HOME/.sv
+rsync -SHPaxq usr/ $HOME/../usr/
 
-export SVDIR=~/.sv
+export SVDIR=$HOME/.sv
 
 sv up tor
 sv up sshd
 
 sleep 1
 
-if [ -f ~/../usr/var/lib/tor/ssh/hostname ]; then
-  cat ~/../usr/var/lib/tor/ssh/hostname
+if [ -f $HOME/../usr/var/lib/tor/ssh/hostname ]; then
+  cat $HOME/../usr/var/lib/tor/ssh/hostname
 fi
+
